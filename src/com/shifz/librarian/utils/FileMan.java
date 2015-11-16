13329dfb14acef49ff16193a1138e7c7ea21e6ba
Shifar Shifz <shifar.shifz@gmail.com>
2015-11-17 00:37:01 +0530

    Constantinized

--- src/com/shifz/librarian/utils/FileMan.java
+++ src/com/shifz/librarian/utils/FileMan.java
@@ -9,12 +9,15 @@ public class FileMan {
 
 
     private static final String[] videoExts = "webm,mkv,flv,vob,ogv,ogg,drc,mng,avi,mov,qt,wmv,rm,rmvb,asf,mp4,m4p,m4v,mpg,mp2,mpeg,mpe,mpv,mpg,mpeg,m2v,m4v,svi,3gp,3g2,mxf,roq,nsv".split(",");
+    private static final CharSequence DOT_SRT = ".srt";
+    private static final CharSequence DOT_STRING = ".";
+    private static final char DOT_CHAR = '.';
 
     public static boolean isVideoFile(final File file) {
 
         if (file.isFile()) {
             final String fileName = file.getName();
-            final String fileExt = fileName.substring(fileName.lastIndexOf('.') + 1);
+            final String fileExt = fileName.substring(fileName.lastIndexOf(DOT_CHAR) + 1);
             for (final String okExt : videoExts) {
                 if (okExt.equals(fileExt)) {
                     return true;
@@ -28,15 +31,15 @@ public class FileMan {
     public static boolean isSubtitleFile(final File file) {
         if (file.isFile()) {
             final String fileName = file.getName();
-            return fileName.contains(".srt") && fileName.substring(fileName.length() - 4).equals(".srt");
+            return fileName.contains(DOT_SRT) && fileName.substring(fileName.length() - DOT_SRT.length()).equals(DOT_SRT);
         }
         return false;
     }
 
 
     public static String getFileNameWithOutExtension(String name) {
-        if (name.contains(".")) {
-            return name.substring(0,name.lastIndexOf('.')) ;
+        if (name.contains(DOT_STRING)) {
+            return name.substring(0, name.lastIndexOf(DOT_CHAR));
         }
         return name;
     }
